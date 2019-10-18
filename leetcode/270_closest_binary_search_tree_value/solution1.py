26ce7c71219efeaac54111f2ad43edba1126be31
Kaitian Xie <xkaitian@gmail.com>
2019-10-17 19:10:51 -0700

    270: Sovle problem using tree traversal

---
+++ leetcode/270_closest_binary_search_tree_value/solution1.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def closestValue(self, root, target):
+        if root is None:
+            return None
+        lower = self.get_lower_bound(root, target)
+        upper = self.get_upper_bound(root, target)
+        if lower is None:
+            return upper.val
+        if upper is None:
+            return lower.val
+        if target - lower.val < upper.val - target:
+            return lower.val
+        return upper.val
+
+    def get_lower_bound(self, root, target):
+        if root is None:
+            return None
+
+        if target < root.val:
+            return self.get_lower_bound(root.left, target)
+        lower = self.get_lower_bound(root.right, target)
+        return root if lower is None else lower
+
+    def get_upper_bound(self, root, target):
+        if root is None:
+            return None
+
+        if target >= root.val:
+            return self.get_upper_bound(root.right, target)
+        upper = self.get_upper_bound(root.left, target)
+        return root if upper is None else upper
