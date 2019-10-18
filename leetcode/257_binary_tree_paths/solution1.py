e7df886f0a8b95f1e9fb708e6a0806e2fdcfbea9
Kaitian Xie <xkaitian@gmail.com>
2019-10-17 20:21:28 -0700

    257: Solve problem using divide & conquer

---
+++ leetcode/257_binary_tree_paths/solution1.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root):
+        if root is None:
+            return []
+
+        if root.left is None and root.right is None:
+            return [str(root.val)]
+
+        left_paths = self.binaryTreePaths(root.left)
+        right_paths = self.binaryTreePaths(root.right)
+
+        paths = []
+        for path in left_paths + right_paths:
+            paths.append(str(root.val) + '->' + path)
+        return paths
