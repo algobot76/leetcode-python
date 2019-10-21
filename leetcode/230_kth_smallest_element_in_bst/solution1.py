2cfb872a7244458bb84c21bcd7bec43561a584b9
Kaitian Xie <xkaitian@gmail.com>
2019-10-20 20:20:20 -0700

    230: Solve problem using iteration

--- leetcode/230_kth_smallest_element_in_bst/solution1.py
+++ leetcode/230_kth_smallest_element_in_bst/solution1.py
@@ -4,7 +4,25 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
+
+from leetcode.utils import TreeNode
+
 
 class Solution:
     def kthSmallest(self, root, k):
-        pass
+        dummy = TreeNode(0)
+        dummy.right = root
+        stack = deque([dummy])
+
+        for i in range(k):
+            node = stack.pop()
+            if node.right:
+                node = node.right
+                while node:
+                    stack.append(node)
+                    node = node.left
+            if not stack:
+                return None
+
+        return stack.pop().val
