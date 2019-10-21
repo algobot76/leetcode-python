65cee2f55dca00fea7072f18f0c129a41f860757
Kaitian Xie <xkaitian@gmail.com>
2019-10-20 20:40:32 -0700

    236: Solve problem using recursion

--- leetcode/236_lowest_common_ancestor_of_a_binary_tree/solution1.py
+++ leetcode/236_lowest_common_ancestor_of_a_binary_tree/solution1.py
@@ -7,4 +7,18 @@
 
 class Solution:
     def lowestCommonAncestor(self, root, p, q):
-        pass
+        if root is None:
+            return None
+        if root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        if left:
+            return left
+        if right:
+            return right
+        return None
