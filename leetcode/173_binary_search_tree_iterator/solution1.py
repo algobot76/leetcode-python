4cef550632d5805b899450a441ec567923218569
Kaitian Xie <xkaitian@gmail.com>
2019-10-27 16:24:29 -0700

    173: Solve problem using stack

--- leetcode/173_binary_search_tree_iterator/solution1.py
+++ leetcode/173_binary_search_tree_iterator/solution1.py
@@ -8,17 +8,28 @@
 class BSTIterator:
 
     def __init__(self, root):
-        pass
+        self.stack = []
+        self.curr = root
 
     def next(self):
         """
         @return the next smallest number
         """
+        while self.curr:
+            self.stack.append(self.curr)
+            self.curr = self.curr.left
+
+        self.curr = self.stack.pop()
+        node = self.curr
+        self.curr = self.curr.right
+
+        return node.val
 
     def hasNext(self):
         """
         @return whether we have a next smallest number
         """
+        return self.stack or self.curr
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
