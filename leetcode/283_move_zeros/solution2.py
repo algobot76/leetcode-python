04b5ecfdaef5d6e0331b616296e26a5c07666e95
Kaitian Xie <xkaitian@gmail.com>
2019-10-29 20:58:12 -0700

    283: Solve problem using two pointers

--- leetcode/283_move_zeros/solution2.py
+++ leetcode/283_move_zeros/solution2.py
@@ -8,6 +8,12 @@ class Solution:
 
         while right < len(nums):
             if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
+                if left != right:
+                    nums[left] = nums[right]
                 left += 1
             right += 1
+
+        while left < len(nums):
+            if nums[left] != 0:
+                nums[left] = 0
+            left += 1
