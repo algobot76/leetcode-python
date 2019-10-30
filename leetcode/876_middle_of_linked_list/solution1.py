0ffd909d4b9173abb7b3a2169455ba0314065eb2
Kaitian Xie <xkaitian@gmail.com>
2019-10-29 20:34:21 -0700

    876: Solve problem using two pointers

--- leetcode/876_middle_of_linked_list/solution1.py
+++ leetcode/876_middle_of_linked_list/solution1.py
@@ -6,4 +6,9 @@
 
 class Solution:
     def middleNode(self, head):
-        pass
+        fast = head
+        slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
