3306a88eef9c292c67145df9924db7d754d3c524
Kaitian Xie <xkaitian@gmail.com>
2019-11-04 20:10:11 -0800

    167: Solve problem using two pointers

--- leetcode/167_two_sum_ii/solution1.py
+++ leetcode/167_two_sum_ii/solution1.py
@@ -1,3 +1,13 @@
 class Solution:
     def twoSum(self, numbers, target):
-        pass
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            sum_ = numbers[left] + numbers[right]
+            if sum_ == target:
+                return [left + 1, right + 1]
+            elif sum_ < target:
+                left += 1
+            else:
+                right -= 1
+
+        return []
