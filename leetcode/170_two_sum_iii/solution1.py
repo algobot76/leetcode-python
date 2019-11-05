52a383d07397a12f31ee1470039c4417a2f6d934
Kaitian Xie <xkaitian@gmail.com>
2019-11-04 20:17:28 -0800

    170: Begin problem

---
+++ leetcode/170_two_sum_iii/solution1.py
@@ -0,0 +1,24 @@
+class TwoSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        pass
+
+    def add(self, number):
+        """
+        Add the number to an internal data structure..
+        """
+        pass
+
+    def find(self, value):
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        """
+        pass
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
