9f0a684173cc7886f4e412702a6819f784486b7c
Kaitian Xie <xkaitian@gmail.com>
2019-11-07 21:27:44 -0800

    78: Finish problem

--- leetcode/78_subsets/solution1.py
+++ leetcode/78_subsets/solution1.py
@@ -1,3 +1,16 @@
 class Solution:
     def subsets(self, nums):
-        pass
+        if not nums:
+            return [nums]
+
+        nums.sort()
+        subsets = []
+        self.dfs(nums, 0, [], subsets)
+        return subsets
+
+    def dfs(self, nums, idx, subset, subsets):
+        subsets.append(list(subset))
+        for i in range(idx, len(nums)):
+            subset.append(nums[i])
+            self.dfs(nums, i + 1, subset, subsets)
+            subset.pop()
