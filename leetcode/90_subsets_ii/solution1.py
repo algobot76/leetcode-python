2b93d6be29366c7a01de7904c49443f694f51a98
Kaitian Xie <xkaitian@gmail.com>
2019-11-07 21:53:09 -0800

    90: Finish problem using dfs

--- leetcode/90_subsets_ii/solution1.py
+++ leetcode/90_subsets_ii/solution1.py
@@ -1,3 +1,15 @@
 class Solution:
     def subsetsWithDup(self, nums):
-        pass
+        nums.sort()
+        subsets = []
+        self.dfs(nums, 0, [], subsets)
+        return subsets
+
+    def dfs(self, nums, idx, subset, subsets):
+        subsets.append(list(subset))
+        for i in range(idx, len(nums)):
+            if i > idx and nums[i] == nums[i - 1]:
+                continue
+            subset.append(nums[i])
+            self.dfs(nums, i + 1, subset, subsets)
+            subset.pop()
