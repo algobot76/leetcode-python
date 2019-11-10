df4b6ee182aa309e85a58494eb41bab0475fe224
Kaitian Xie <xkaitian@gmail.com>
2019-11-09 21:12:29 -0800

    40: Finish problem using dfs

--- leetcode/40_combination_sum_ii/solution1.py
+++ leetcode/40_combination_sum_ii/solution1.py
@@ -1,3 +1,22 @@
 class Solution:
     def combinationSum2(self, candidates, target):
-        pass
+        candidates.sort()
+        combs = []
+        self.dfs(candidates, target, 0, [], combs)
+        return combs
+
+    def dfs(self, candidates, target, start, comb, combs):
+        if target < 0:
+            return
+        if target == 0:
+            return combs.append(list(comb))
+
+        prev = 0
+        while start < len(candidates) and candidates[start] <= target:
+            if prev != candidates[start]:
+                comb.append(candidates[start])
+                self.dfs(candidates, target - candidates[start], start + 1,
+                         comb, combs)
+                comb.pop()
+                prev = candidates[start]
+            start += 1
