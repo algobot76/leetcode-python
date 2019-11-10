4cd916d95ab3e52f68ce4fa43a9833a282b804dd
Kaitian Xie <xkaitian@gmail.com>
2019-11-09 23:36:48 -0800

    17: Solve problem using DFS

--- leetcode/17_letter_combinations_of_a_phone_number/solution1.py
+++ leetcode/17_letter_combinations_of_a_phone_number/solution1.py
@@ -1,3 +1,28 @@
+MAPPING = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+}
+
+
 class Solution:
     def letterCombinations(self, digits):
-        pass
+        if not digits:
+            return []
+
+        ans = []
+        self.dfs(digits, 0, '', ans)
+        return ans
+
+    def dfs(self, digits, idx, comb, ans):
+        if idx == len(digits):
+            ans.append(comb)
+            return
+
+        for letter in MAPPING[digits[idx]]:
+            self.dfs(digits, idx + 1, comb + letter, ans)
