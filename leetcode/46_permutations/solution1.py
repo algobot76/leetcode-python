d8425e618dae1823274c1556cf6c0d17aacb4beb
Kaitian Xie <xkaitian@gmail.com>
2019-11-10 00:07:17 -0800

    46: Solve problem using DFS

--- leetcode/46_permutations/solution1.py
+++ leetcode/46_permutations/solution1.py
@@ -1,3 +1,18 @@
 class Solution:
     def permute(self, nums):
-        pass
+        if nums is None:
+            return []
+        if len(nums) == 0:
+            return [[]]
+
+        nums.sort()
+        ans = []
+        self.dfs(nums, [], ans)
+        return ans
+
+    def dfs(self, nums, perm, ans):
+        if len(nums) == 0:
+            ans.append(perm)
+        else:
+            for i in range(len(nums)):
+                self.dfs(nums[:i] + nums[i + 1:], perm + [nums[i]], ans)
