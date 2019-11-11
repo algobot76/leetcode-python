24697b5220e80e25606c63ffdae25fcb09e00e79
Kaitian Xie <xkaitian@gmail.com>
2019-11-11 14:55:04 -0800

    51: Solve problem using DFS

--- leetcode/51_n_queens/solution1.py
+++ leetcode/51_n_queens/solution1.py
@@ -1,3 +1,16 @@
 class Solution:
     def solveNQueens(self, n):
-        pass
+        ans = []
+        self.dfs(n, [], [], [], ans)
+        return [['.' * i + 'Q' + '.' * (n - i - 1) for i in result] for result
+                in ans]
+
+    def dfs(self, n, col_per_row, xy_diff, xy_sum, ans):
+        cur_row = len(col_per_row)
+        if cur_row == n:
+            ans.append(col_per_row)
+        for col in range(n):
+            if col not in col_per_row and (cur_row - col not in xy_diff) and (
+                    cur_row + col not in xy_sum):
+                self.dfs(n, col_per_row + [col], xy_diff + [cur_row - col],
+                         xy_sum + [cur_row + col], ans)
