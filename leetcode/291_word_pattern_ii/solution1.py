dc1936e3c25637094d00370283dfdeb771a40c98
Kaitian Xie <xkaitian@gmail.com>
2019-11-11 15:14:42 -0800

    291: Solve problem using DFS

--- leetcode/291_word_pattern_ii/solution1.py
+++ leetcode/291_word_pattern_ii/solution1.py
@@ -1,3 +1,27 @@
 class Solution:
     def wordPatternMatch(self, pattern, str):
-        pass
+        return self.match(pattern, str, {}, set())
+
+    def match(self, pattern, string, mapping, used):
+        if not pattern:
+            return not string
+
+        char = pattern[0]
+        if char in mapping:
+            word = mapping[char]
+            if not string.startswith(word):
+                return False
+            return self.match(pattern[1:], string[len(word):], mapping, used)
+
+        for i in range(len(string)):
+            word = string[:i + 1]
+            if word in used:
+                continue
+            used.add(word)
+            mapping[char] = word
+            if self.match(pattern[1:], string[i + 1:], mapping, used):
+                return True
+            del mapping[char]
+            used.remove(word)
+
+        return False
