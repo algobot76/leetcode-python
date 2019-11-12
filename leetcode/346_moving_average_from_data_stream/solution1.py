42690fc0adb5e500efb1b3d34425f8655ba82695
Kaitian Xie <xkaitian@gmail.com>
2019-11-12 05:03:51 -0800

    346: Begin problem

---
+++ leetcode/346_moving_average_from_data_stream/solution1.py
@@ -0,0 +1,13 @@
+class MovingAverage:
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        """
+
+    def next(self, val):
+        pass
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
