0959547958247b4ff6a00da8050d32c72cad95f7
Kaitian Xie <xkaitian@gmail.com>
2019-11-12 07:03:15 -0800

    23: Solve problem using recurrsion

--- leetcode/23_merge_k_sorted_lists/solution1.py
+++ leetcode/23_merge_k_sorted_lists/solution1.py
@@ -3,7 +3,37 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+from leetcode.utils import ListNode
+
 
 class Solution:
     def mergeKLists(self, lists):
-        pass
+        if not lists:
+            return None
+
+        return self.helper(lists, 0, len(lists) - 1)
+
+    def merge_two_lists(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+
+        curr.next = l1 or l2
+        return dummy.next
+
+    def helper(self, lists, start, end):
+        if start > end:
+            return None
+        if start == end:
+            return lists[start]
+        return self.merge_two_lists(
+            self.helper(lists, start, (start + end) // 2),
+            self.helper(lists, (start + end) // 2 + 1, end))
--- leetcode/utils.py
+++ leetcode/utils.py
@@ -2,6 +2,12 @@ def isBadVersion(version):
     return version % 2 == 0
 
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
