b61ab9552576a6873220451d791af6760d4a385c
Kaitian Xie <xkaitian@gmail.com>
2019-11-11 21:03:57 -0800

    973: Solve problem using priority queue

--- leetcode/973_k_closest_points_to_origin/solution1.py
+++ leetcode/973_k_closest_points_to_origin/solution1.py
@@ -1,3 +1,23 @@
+from heapq import heappop
+from heapq import heappush
+
+
 class Solution:
     def kClosest(self, points, K):
-        pass
+        pq = []
+        for point in points:
+            dist = self.get_distance(point)
+            heappush(pq, (-dist, -point[0], -point[1]))
+            if len(pq) > K:
+                heappop(pq)
+
+        ans = []
+        while len(pq) > 0:
+            (_, x, y) = heappop(pq)
+            ans.append([-x, -y])
+        return ans
+
+    def get_distance(self, point):
+        x = point[0]
+        y = point[1]
+        return x ** 2 + y ** 2
