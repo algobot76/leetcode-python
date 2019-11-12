f3c0322e2fc00b81d2d6b27556de10956b2c88fc
Kaitian Xie <xkaitian@gmail.com>
2019-11-12 05:58:09 -0800

    225: Begin problem

---
+++ leetcode/225_implement_stack_using_queues/solution1.py
@@ -0,0 +1,33 @@
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        """
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+
+    def top(self):
+        """
+        Get the top element.
+        """
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        """
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
