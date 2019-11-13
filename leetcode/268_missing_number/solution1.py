5fd8a143b0a3ecfedb4f35e13b02ef23fea97bde
Kaitian Xie <xkaitian@gmail.com>
2019-11-13 05:19:55 -0800

    268: Solve problem using map

--- leetcode/268_missing_number/solution1.py
+++ leetcode/268_missing_number/solution1.py
@@ -1,3 +1,15 @@
+import collections
+
+
 class Solution:
     def missingNumber(self, nums):
-        pass
+        n = len(nums) + 1
+
+        counts = collections.defaultdict(int)
+        for num in nums:
+            counts[num] += 1
+        for i in range(n + 1):
+            if counts[i] == 0:
+                return i
+
+        return -1
