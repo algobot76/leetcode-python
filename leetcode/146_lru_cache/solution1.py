a5c32013fd3c98c312d2db2f6d0157a262b806a6
Kaitian Xie <xkaitian@gmail.com>
2019-11-13 04:29:44 -0800

    146: Begin problem

---
+++ leetcode/146_lru_cache/solution1.py
@@ -0,0 +1,15 @@
+class LRUCache:
+
+    def __init__(self, capacity):
+        pass
+
+    def get(self, key):
+        pass
+
+    def put(self, key, value):
+        pass
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
