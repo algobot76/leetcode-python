e7763934b14f3aeac6c40dc38cff118ac0c6891d
Kaitian Xie <xkaitian@gmail.com>
2019-11-13 04:22:06 -0800

    264: Solve problem using priority queue

--- leetcode/264_ugly_number_ii/solution1.py
+++ leetcode/264_ugly_number_ii/solution1.py
@@ -1,3 +1,18 @@
+import heapq
+
+
 class Solution:
     def nthUglyNumber(self, n):
-        pass
+        pq = [1]
+        visited = {1}
+
+        res = None
+        for i in range(n):
+            res = heapq.heappop(pq)
+            for factor in [2, 3, 5]:
+                num = res * factor
+                if num not in visited:
+                    visited.add(num)
+                    heapq.heappush(pq, num)
+
+        return res
