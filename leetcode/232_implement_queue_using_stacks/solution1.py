4bb62de7e0c6c98b44755245890142d336652108
Kaitian Xie <xkaitian@gmail.com>
2019-11-13 04:02:47 -0800

    232: Begin problem

---
+++ leetcode/232_implement_queue_using_stacks/solution1.py
@@ -0,0 +1,33 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        """
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+
+    def peek(self):
+        """
+        Get the front element.
+        """
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        """
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
