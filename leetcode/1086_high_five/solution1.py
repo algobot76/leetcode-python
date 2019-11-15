51c736f24e1f34fcf65255cbcec740fe3947407d
Kaitian Xie <xkaitian@gmail.com>
2019-11-14 22:59:06 -0800

    1086: Solve problem using priority queue

--- leetcode/1086_high_five/solution1.py
+++ leetcode/1086_high_five/solution1.py
@@ -1,3 +1,22 @@
+import collections
+import heapq
+
+
 class Solution:
     def highFive(self, items):
-        pass
+        students = collections.defaultdict(list)
+        for item in items:
+            students[item[0]].append(item[1])
+
+        averages = []
+        for id_, scores in students.items():
+            top_five = sorted(scores, reverse=True)[:5]
+            avg = sum(top_five) // len(top_five)
+            heapq.heappush(averages, (id_, -avg))
+
+        ans = []
+        while len(averages) > 0:
+            avg = heapq.heappop(averages)
+            ans.append([avg[0], -avg[1]])
+
+        return ans
