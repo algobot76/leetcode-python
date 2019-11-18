752c9e07f4c2d963bc149d02821b0a208f2b6cbc
Kaitian Xie <xkaitian@gmail.com>
2019-11-17 16:55:57 -0800

    937: Solve problem

--- leetcode/937_reorder_data_in_log_files/solution1.py
+++ leetcode/937_reorder_data_in_log_files/solution1.py
@@ -1,3 +1,11 @@
 class Solution:
     def reorderLogFiles(self, logs):
-        pass
+        if not logs:
+            return []
+
+        letter_logs = [log for log in logs if
+                       log[log.index(' ') + 1].isalpha()]
+        digit_logs = [log for log in logs if log[log.index(' ') + 1].isdigit()]
+        letter_logs.sort(
+            key=lambda log: (' '.join(log.split()[1:]), log.split()[0]))
+        return letter_logs + digit_logs
