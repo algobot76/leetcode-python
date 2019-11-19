0bc2885ec8250f5fd7f57dd7a85f2fbe949a1120
Kaitian Xie <xkaitian@gmail.com>
2019-11-18 23:12:23 -0800

    763: Solve problem uisng brute forece

--- leetcode/763_partition_labels/solution1.py
+++ leetcode/763_partition_labels/solution1.py
@@ -1,3 +1,12 @@
 class Solution:
     def partitionLabels(self, S):
-        pass
+        left = 0
+        right = 0
+        ans = []
+        for i in range(len(S)):
+            right = max(right, S.rfind(S[i]))
+            if i == right:
+                ans.append(right - left + 1)
+                left = right + 1
+
+        return ans
