f1555199837785a940fc383c82db853868ddd71e
Kaitian Xie <xkaitian@gmail.com>
2019-11-19 20:10:22 -0800

    240: Solve problem using two pointers

---
+++ leetcode/240_search_a_2d_matrix_ii/solution2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        if not matrix or len(matrix[0]) == 0:
+            return False
+
+        m = len(matrix)
+        n = len(matrix[0])
+        i = 0
+        j = n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+
+        return False
