5d0dc31b9d12feb3492e1b729628cebf9aadbc35
Kaitian Xie <xkaitian@gmail.com>
2019-11-19 20:15:07 -0800

    240: Add comments

--- leetcode/240_search_a_2d_matrix_ii/solution2.py
+++ leetcode/240_search_a_2d_matrix_ii/solution2.py
@@ -1,5 +1,10 @@
 class Solution:
     def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
         if not matrix or len(matrix[0]) == 0:
             return False
 
