4c9270d9d2db7630c0331a89b708642725354b6d
Kaitian Xie <xkaitian@gmail.com>
2019-11-21 00:36:39 -0800

    642: Begin problem

---
+++ leetcode/642_design_search_autocomplete_system/solution1.py
@@ -0,0 +1,11 @@
+class AutocompleteSystem:
+
+    def __init__(self, sentences, times):
+        pass
+
+    def input(self, c):
+        pass
+
+# Your AutocompleteSystem object will be instantiated and called as such:
+# obj = AutocompleteSystem(sentences, times)
+# param_1 = obj.input(c)
