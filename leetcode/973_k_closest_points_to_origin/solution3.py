e9e94da61701ef41ef65cc71a3e825d9088d30e2
Kaitian Xie <xkaitian@gmail.com>
2020-01-22 15:42:09 -0800

    53: Rewrite solution 3

--- leetcode/973_k_closest_points_to_origin/solution3.py
+++ leetcode/973_k_closest_points_to_origin/solution3.py
@@ -4,36 +4,35 @@ from typing import List
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
 
-        def get_distance(i: int):
-            return points[i][0] ** 2 + points[i][1] ** 2
-
-        def sort(left: int, right: int, K: int):
-            if left >= right:
-                return
-
-            pivot_idx = (left + right) // 2
-            points[left], points[pivot_idx] = points[pivot_idx], points[left]
-            mid = partition(left, right)
-
-            if K < mid - left + 1:
-                sort(left, mid - 1, K)
-            elif K > mid - left + 1:
-                sort(mid + 1, right, K - (mid - left + 1))
-
-        def partition(left: int, right: int):
-            l = left
-            r = right
-            pivot = get_distance(l)
-
-            while l < r:
-                while l < r and get_distance(r) >= pivot:
-                    r -= 1
-                while l < r and get_distance(l) <= pivot:
-                    l += 1
-                points[l], points[r] = points[r], points[l]
-
-            points[left], points[r] = points[r], points[left]
-            return r
-
-        sort(0, len(points) - 1, K)
+        self.sort(points, 0, len(points) - 1, K)
         return points[:K]
+
+    def sort(self, points: List[List[int]], left: int, right: int, K: int):
+        if left >= right:
+            return
+
+        pivot_idx = self.partition(points, left, right)
+
+        if K < pivot_idx - left + 1:
+            self.sort(points, left, pivot_idx - 1, K)
+        elif K > pivot_idx - left + 1:
+            self.sort(points, pivot_idx + 1, right, K - (pivot_idx - left + 1))
+
+    def partition(self, points: List[List[int]], left: int, right: int):
+        pivot = self.get_dist(points, left)
+        i = left
+        j = right
+
+        while i < j:
+            while i <= right and self.get_dist(points, i) <= pivot:
+                i += 1
+            while self.get_dist(points, j) > pivot:
+                j -= 1
+            if i < j:
+                points[i], points[j] = points[j], points[i]
+
+        points[left], points[j] = points[j], points[left]
+        return j
+
+    def get_dist(self, points: List[List[int]], i: int) -> int:
+        return points[i][0] ** 2 + points[i][1] ** 2
