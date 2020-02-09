19e7c3eb8d23590cdd0c99cf10b885315e8ad768
Kaitian Xie <xkaitian@gmail.com>
2020-02-09 11:27:25 -0800

    1: Solve problem using one-pass hash table

---
+++ leetcode/1_two_sum/solution3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in d:
+                return [i, d[complement]]
+
+            d[nums[i]] = i
+
+        return -1, -1
