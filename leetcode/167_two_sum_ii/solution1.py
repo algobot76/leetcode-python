e1ae5abb6889fabbdbd29178f670bb59a8dc5083
Kaitian Xie <xkaitian@gmail.com>
2020-02-15 19:42:47 -0800

    167: Update solution 1

--- leetcode/167_two_sum_ii/solution1.py
+++ leetcode/167_two_sum_ii/solution1.py
@@ -1,13 +1,15 @@
 class Solution:
-    def twoSum(self, numbers, target):
-        left, right = 0, len(numbers) - 1
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
         while left < right:
             sum_ = numbers[left] + numbers[right]
             if sum_ == target:
                 return [left + 1, right + 1]
-            elif sum_ < target:
-                left += 1
-            else:
+            elif sum_ > target:
                 right -= 1
+            else:
+                left += 1
 
-        return []
+        return [-1, -1]
