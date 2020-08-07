efed935ff64d2157e29f32ac278c4fcf6ec59626
Kaitian Xie <xkaitian@gmail.com>
2020-08-06 17:33:12 -0700

    91: Rename num_ways as ok

--- leetcode/91_decode_ways/solution1.py
+++ leetcode/91_decode_ways/solution1.py
@@ -5,15 +5,15 @@ class Solution:
 
         n = len(s)
         f = [1, 0, 0]
-        f[1] = self.num_ways(s[0])
+        f[1] = self.ok(s[0])
 
         for i in range(2, n + 1):
-            f[i % 3] = f[(i - 1) % 3] * self.num_ways(s[i - 1:i]) + f[
-                (i - 2) % 3] * self.num_ways(s[i - 2:i])
+            f[i % 3] = f[(i - 1) % 3] * self.ok(s[i - 1:i]) + f[
+                (i - 2) % 3] * self.ok(s[i - 2:i])
 
         return f[n % 3]
 
-    def num_ways(self, s):
+    def ok(self, s):
         code = int(s)
         if len(s) == 1 and 1 <= code <= 9:
             return 1
