62d9870589dff72f66f5920c5d3f04690ed27935
Kaitian Xie <xkaitian@gmail.com>
2020-08-10 14:13:50 -0700

    198: Add an optimized DP solution

--- leetcode/198_house_robber/solution1.py
+++ leetcode/198_house_robber/solution1.py
@@ -5,13 +5,12 @@ class Solution:
             return 0
         if n == 1:
             return nums[0]
-        if n == 2:
-            return max(nums[0], nums[1])
 
-        f = [0] * n
-        f[0] = nums[0]
-        f[1] = max(nums[0], nums[1])
-        for i in range(2, n):
-            f[i] = max(f[i - 2] + nums[i], f[i - 1])
+        prev_sum = 0
+        curr_sum = 0
+        for num in nums:
+            temp = curr_sum
+            curr_sum = max(prev_sum + num, curr_sum)
+            prev_sum = temp
 
-        return f[n - 1]
+        return curr_sum
