997c56546cbf92e124da290d9d9419f37b8403fd
Kaitian Xie <xkaitian@gmail.com>
2020-08-11 16:40:37 -0700

    75: Solve problem using two pointers

---
+++ leetcode/75_sort_colors/solution1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortColors(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right = len(nums) - 1
+        mid = left
+
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+            else:
+                mid += 1
