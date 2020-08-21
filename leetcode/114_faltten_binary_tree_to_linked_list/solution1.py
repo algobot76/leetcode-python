48985287a2a36dca5d70d03bd5099f9d73b386ce
Kaitian Xie <xkaitian@gmail.com>
2020-08-21 14:37:12 -0700

    114: Refactor solution1

--- leetcode/114_faltten_binary_tree_to_linked_list/solution1.py
+++ leetcode/114_faltten_binary_tree_to_linked_list/solution1.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def __init__(self):
         self.last_node = None
@@ -13,10 +14,10 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        if root is None:
+        if not root:
             return
 
-        if self.last_node is not None:
+        if self.last_node:
             self.last_node.left = None
             self.last_node.right = root
 
